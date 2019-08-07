0988563585c430c44e4534d47a293348a39d003a
sklaw <sk.law.lsq@gmail.com>
2019-08-07 19:18:48 -0400

    saving

--- scripts/spawn_camera_and_table_in_gazebo.py
+++ scripts/spawn_camera_and_table_in_gazebo.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+from baxter_kitting_experiment_using_gps import CONSTANT
+import os
+import subprocess
+import rospy
+from gazebo_msgs.srv import DeleteModel, DeleteModelRequest
+
+def setup_experiment_table():
+    delete_model_service = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
+    delete_model_service.call(DeleteModelRequest(model_name='experiment_table'))
+
+    table_urdf_xacro = os.path.join(CONSTANT.gazebo_model_dir, 'table.urdf.xacro')
+    table_urdf = subprocess.check_output("rosrun xacro xacro %s"%table_urdf_xacro, shell=True)
+    rospy.set_param("table_urdf", table_urdf)
+    subprocess.call("rosrun gazebo_ros spawn_model -urdf -param table_urdf -model experiment_table", shell=True)
 
 if __name__ == '__main__':
-    pass
+    rospy.init_node('spawn_camera_and_table_in_gazebo_node')
+    setup_experiment_table()
---
+++ src/baxter_kitting_experiment_using_gps/CONSTANT.py
@@ -0,0 +1,5 @@
+import os
+
+dir_of_this_script = os.path.realpath(os.path.dirname(__file__))
+gazebo_model_dir = os.path.join(dir_of_this_script, '..', '..', 'gazebo_model_files')
+ 

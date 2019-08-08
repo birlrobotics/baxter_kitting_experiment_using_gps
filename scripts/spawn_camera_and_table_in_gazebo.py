b4c686464792685dc01b5c174655f8284cd3e7a2
sklaw <sk.law.lsq@gmail.com>
2019-08-08 11:06:40 -0400

    new camera pose

--- scripts/spawn_camera_and_table_in_gazebo.py
+++ scripts/spawn_camera_and_table_in_gazebo.py
@@ -8,6 +8,7 @@ from gazebo_msgs.srv import DeleteModel, DeleteModelRequest
 from tf.transformations import euler_matrix, euler_from_matrix
 import numpy as np
 import math
+import signal
 
 delete_model_service = None
 
@@ -26,8 +27,8 @@ def setup_camera():
     camera_sdf = os.path.join(CONSTANT.gazebo_model_dir, 'camera.sdf')
     delete_model_service.call(DeleteModelRequest(model_name=model_name))
 
-    x, y, z = [0.3, 0, 0.2]
-    roll, pitch, yaw = [0, 0.65, 0]
+    x, y, z = [1.5, -0.5, 0.2]
+    roll, pitch, yaw = [0, math.pi/8, math.pi*6.5/8]
 
     subprocess.call("rosrun gazebo_ros spawn_model -sdf -file %s -model %s -reference_frame baxter::base -x %s -y %s -z %s -R %s -P %s -Y %s"%(camera_sdf, model_name, x, y, z, roll, pitch, yaw,), shell=True)
 
@@ -36,7 +37,9 @@ def setup_camera():
 
     tf_roll, tf_pitch, tf_yaw = euler_from_matrix(np.dot(euler_matrix(roll, pitch, yaw), euler_matrix(*rviz_frame_correction_rpy)))
 
-    subprocess.Popen('rosrun tf static_transform_publisher %s %s %s %s %s %s /base /baxter_kitting_expertiment_camera_frame 1000'%(x, y+0.06, z, tf_yaw, tf_pitch, tf_roll,), shell=True)
+    tf_pub_process = subprocess.Popen('rosrun tf static_transform_publisher %s %s %s %s %s %s /base /baxter_kitting_expertiment_camera_frame 1000'%(x, y, z, tf_yaw, tf_pitch, tf_roll,), shell=True)
+
+    return tf_pub_process
 
 def setup_object():
     model_name = 'experiment_box'
@@ -45,13 +48,17 @@ def setup_object():
     box_urdf_xacro = os.path.join(CONSTANT.gazebo_model_dir, 'box.urdf.xacro')
     box_urdf = subprocess.check_output("rosrun xacro xacro %s"%box_urdf_xacro, shell=True)
     rospy.set_param("box_urdf", box_urdf)
-    subprocess.call("rosrun gazebo_ros spawn_model -urdf -param box_urdf -model %s -reference_frame baxter::base -x 0.7 -y 0 -z 1"%(model_name,), shell=True)
+    subprocess.call("rosrun gazebo_ros spawn_model -urdf -param box_urdf -model %s -reference_frame baxter::base -x 0.85 -y 0 -z 1"%(model_name,), shell=True)
 
 if __name__ == '__main__':
     rospy.init_node('spawn_camera_and_table_in_gazebo_node')
     delete_model_service = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
     setup_experiment_table()
-    setup_camera()
+    tf_pub_process = setup_camera()
     setup_object()
 
     rospy.spin()
+
+    os.kill(tf_pub_process.pid, signal.SIGINT)
+    os.kill(tf_pub_process.pid, signal.SIGTERM)
+    

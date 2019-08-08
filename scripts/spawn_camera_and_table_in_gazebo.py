3d738b774fa6a0a356dca4eec8a1b978554997d4
sklaw <sk.law.lsq@gmail.com>
2019-08-07 20:16:19 -0400

    done spawning table

--- scripts/spawn_camera_and_table_in_gazebo.py
+++ scripts/spawn_camera_and_table_in_gazebo.py
@@ -13,7 +13,7 @@ def setup_experiment_table():
     table_urdf_xacro = os.path.join(CONSTANT.gazebo_model_dir, 'table.urdf.xacro')
     table_urdf = subprocess.check_output("rosrun xacro xacro %s"%table_urdf_xacro, shell=True)
     rospy.set_param("table_urdf", table_urdf)
-    subprocess.call("rosrun gazebo_ros spawn_model -urdf -param table_urdf -model experiment_table", shell=True)
+    subprocess.call("rosrun gazebo_ros spawn_model -urdf -param table_urdf -model experiment_table -reference_frame baxter::base -x 0 -y 0 -z -0.7", shell=True)
 
 if __name__ == '__main__':
     rospy.init_node('spawn_camera_and_table_in_gazebo_node')

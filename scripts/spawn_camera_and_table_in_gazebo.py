43e2c65ad6eb90b29c6eb7757687116418361bc3
sklaw <sk.law.lsq@gmail.com>
2019-08-08 10:14:37 -0400

    saving

--- scripts/spawn_camera_and_table_in_gazebo.py
+++ scripts/spawn_camera_and_table_in_gazebo.py
@@ -9,9 +9,10 @@ from tf.transformations import euler_matrix, euler_from_matrix
 import numpy as np
 import math
 
+delete_model_service = None
+
 def setup_experiment_table():
     model_name = 'experiment_table'
-    delete_model_service = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
     delete_model_service.call(DeleteModelRequest(model_name=model_name))
 
     table_urdf_xacro = os.path.join(CONSTANT.gazebo_model_dir, 'table.urdf.xacro')
@@ -21,7 +22,6 @@ def setup_experiment_table():
 
 def setup_camera():
     model_name = 'experiment_camera'
-    delete_model_service = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
 
     camera_sdf = os.path.join(CONSTANT.gazebo_model_dir, 'camera.sdf')
     delete_model_service.call(DeleteModelRequest(model_name=model_name))
@@ -38,10 +38,20 @@ def setup_camera():
 
     subprocess.Popen('rosrun tf static_transform_publisher %s %s %s %s %s %s /base /baxter_kitting_expertiment_camera_frame 1000'%(x, y+0.06, z, tf_yaw, tf_pitch, tf_roll,), shell=True)
 
+def setup_object():
+    model_name = 'experiment_box'
+    delete_model_service.call(DeleteModelRequest(model_name=model_name))
+
+    box_urdf_xacro = os.path.join(CONSTANT.gazebo_model_dir, 'box.urdf.xacro')
+    box_urdf = subprocess.check_output("rosrun xacro xacro %s"%box_urdf_xacro, shell=True)
+    rospy.set_param("box_urdf", box_urdf)
+    subprocess.call("rosrun gazebo_ros spawn_model -urdf -param box_urdf -model %s -reference_frame baxter::base -x 0.7 -y 0 -z 1"%(model_name,), shell=True)
 
 if __name__ == '__main__':
     rospy.init_node('spawn_camera_and_table_in_gazebo_node')
+    delete_model_service = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
     setup_experiment_table()
     setup_camera()
+    setup_object()
 
     rospy.spin()

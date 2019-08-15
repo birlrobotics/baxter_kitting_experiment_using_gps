bff000333b6525ca24219a200ace01c1b6a89e1a
sklaw <sk.law.lsq@gmail.com>
2019-08-15 11:48:36 -0400

    saving

--- test/test_image_subscriber.py
+++ test/test_image_subscriber.py
@@ -4,15 +4,16 @@ from sensor_msgs.msg import CompressedImage
 from PIL import Image
 import io
 import numpy as np
+from matplotlib import pyplot as plt
 
-def cb(msg):
-    image = Image.open(io.BytesIO(msg.data))
-    image_mat = np.array(image)
-    pdb.set_trace()
+
 
 if __name__ == '__main__':
     rospy.init_node('test_node')
 
-    rospy.Subscriber('/baxter_kitting_expertiment_camera/image_raw/compressed', CompressedImage, cb)
-
-    rospy.spin()
+    while not rospy.is_shutdown():
+        msg = rospy.wait_for_message('/gps_rgb_camera/image_raw/compressed', CompressedImage)
+        image = Image.open(io.BytesIO(msg.data))
+        image.show()
+        print 'hit enter to get next frame'
+        raw_input()
